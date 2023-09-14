5dc093510969a7992c7744126a24b1bbbc8a5ee7
BeeSoftwareMahmut <arimahmut2314@gmail.com>
2023-09-14 18:37:37 +0300

    app reloaded

--- src/App.js
+++ src/App.js
@@ -11,20 +11,13 @@ function App() {
 
 
   return (
-   
-      
-   <BrowserRouter>
-   <Routes>
-   <ToastContainer/>
-   <ContextProvider>
-    <Route exact path="/" element={<Main/>} />
-    <Route path="/about" element={<About/>} />
-    <Route path="/services" element={<Services/>} />
-    <Route path="/contact" element={<Contact/>}/>
-    <Route path="/admin" element={<Admin/>}/>
-    </ContextProvider>
-   </Routes>
-   </BrowserRouter>
+    
+    <ContextProvider>
+      <div>
+        <ToastContainer/>
+   <Main/>
+   </div>
+   </ContextProvider>
   
 
   );
