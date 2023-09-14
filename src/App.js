83605c595e5a579339d32e7846606da0bbc0fb22
BeeSoftwareMahmut <arimahmut2314@gmail.com>
2023-09-14 21:00:51 +0300

    app uploaded 2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import About from "./Components/About/About";
-import Main from "./Components/Main";
-import Services from "./Components/Services/Service";
-import Contact from "./Components/Contact/Contact";
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Admin from "./Components/Admin/Admin";
-import { ContextProvider} from "./Components/Context/Context";
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Main from './Components/Main';
+import About from './Components/About/About';
+import Services from './Components/Services/Service';
+import Contact from './Components/Contact/Contact';
+import Admin from './Components/Admin/Admin';
+import { ContextProvider } from './Components/Context/Context';
 import { ToastContainer } from 'react-toastify';
 
 function App() {
-
-
   return (
-    
     <ContextProvider>
-      <div>
-        <ToastContainer/>
-   <Main/>
-   </div>
-   </ContextProvider>
-  
-
+      <Router>
+        <div>
+          <ToastContainer />
+          {/* Ana sayfa için / adresine yönlendirme */}
+          <Route path="/" exact component={Main} />
+          {/* Hakkında sayfası için /about adresine yönlendirme */}
+          <Route path="/about" component={About} />
+          {/* Hizmetler sayfası için /services adresine yönlendirme */}
+          <Route path="/services" component={Services} />
+          {/* İletişim sayfası için /contact adresine yönlendirme */}
+          <Route path="/contact" component={Contact} />
+          {/* Admin sayfası için /admin adresine yönlendirme */}
+          <Route path="/admin" component={Admin} />
+        </div>
+      </Router>
+    </ContextProvider>
   );
 }
 
